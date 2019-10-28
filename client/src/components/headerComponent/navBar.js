f91027a2c574e06ea9172c679c9923639f7395d7
labenh526 <labenh526@gmail.com>
2019-10-28 14:55:22 -0400

    added component files

---
+++ client/src/components/headerComponent/navBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+import logo from './logo.png';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <header>
+        {/* <ul id="headerButtons">
+          <li className="navButton"><Link to="">Home</Link></li>
+          <li className="navButton"><Link to="">Help</Link></li>
+        </ul> */}
+        <b><font size="12">Tortoise</font></b>
+        <img src={logo} width={50} height={50} mode='fit'/>
+      </header>
+    )
+  }
+}
+
+export default NavBar;
---
+++ client/src/components/pages/helpPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HelpPage extends Component {
+  render() {
+    return (
+      <div>
+        Help page
+      </div>
+    )
+  }
+}
+
+export default HelpPage;
