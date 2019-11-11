33138ff62156213e58e77228f220326073c634fa
Vincent Dong <vyd4@case.edu>
2019-11-11 03:02:27 -0500

    accidentally removed code that linked to main app

--- client/src/App.js
+++ client/src/App.js
@@ -75,6 +75,9 @@ class App extends Component{
       <div> 
         <img src={this.state.nowPlaying.image } style = {{widows: 100}}/>
       </div>
+      <div>
+        <Recommendations spotifyApi={spotifyWebApi} />
+      </div>
       <Button onClick={() => this.getNowPlaying()}> 
         Check Now Playing
       </Button>
