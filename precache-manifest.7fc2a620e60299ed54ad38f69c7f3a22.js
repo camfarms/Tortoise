bbaedbaa449c46e947455789d50657234743022a
camfarms <ccf35@case.edu>
2019-12-04 15:18:41 -0500

    Updates

---
+++ precache-manifest.7fc2a620e60299ed54ad38f69c7f3a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb501d6073bdbd022b31be7f8b6a734",
+    "url": "/EECS398_will_this_work_pls/index.html"
+  },
+  {
+    "revision": "14e6145b26750abd6079",
+    "url": "/EECS398_will_this_work_pls/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "bf2d67190a7f77a9e940",
+    "url": "/EECS398_will_this_work_pls/static/css/main.aed36a2a.chunk.css"
+  },
+  {
+    "revision": "14e6145b26750abd6079",
+    "url": "/EECS398_will_this_work_pls/static/js/2.03379681.chunk.js"
+  },
+  {
+    "revision": "bf2d67190a7f77a9e940",
+    "url": "/EECS398_will_this_work_pls/static/js/main.67ba5973.chunk.js"
+  },
+  {
+    "revision": "8478acd69094929496c8",
+    "url": "/EECS398_will_this_work_pls/static/js/runtime-main.94be3a04.js"
+  },
+  {
+    "revision": "b97c5779ae783ea517d93dd4d2a95a0c",
+    "url": "/EECS398_will_this_work_pls/static/media/tortoise_white.b97c5779.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/EECS398_will_this_work_pls/precache-manifest.7fc2a620e60299ed54ad38f69c7f3a22.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EECS398_will_this_work_pls/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
