9d910800553a3ddae5cec5e906f61739d6cd91db
labenh526 <labenh526@gmail.com>
2019-12-03 21:16:27 -0500

    Added more testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,15 +2,16 @@ import React from 'react';
 import App from './App.js';
 import ArtistProfile from './ArtistProfile.js';
 import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
 import {render, fireEvent, getByRole} from '@testing-library/react';
 import {shallow, configure, mount} from 'enzyme';
-import { jssPreset } from '@material-ui/core';
-import Spotify from 'spotify-web-api-js';
+import Lyrics from './Lyrics.js';
+import RecommendationsTable from './Recommendations/Recommendations.js';
+
 
 /* Objects/Configurations necessary for tests */
 configure({adapter: new Adapter()});
 let pushatdesc = "Terrence LeVarr Thornton (born May 13, 1977), better known by his stage name Pusha T, is an American rapper, songwriter and record executive. He initially gained major recognition as half of hip hop duo Clipse, alongside his brother and fellow rapper No Malice, with whom he founded Re-Up Records. In September 2010, Thornton announced his signing to Kanye West's GOOD Music imprint, under the aegis of Def Jam Recordings. In March 2011, he released his first solo project, a mixtape titled Fear of God. Thornton released his debut solo album, My Name Is My Name, in October 2013. In November 2015, Kanye West appointed Pusha T to take over his role as president of GOOD Music.";
+const blueURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEUAAP+KeNJXAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC";
 
 //Create mock object for spotify api
 const spotifyMock = {getMyCurrentPlaybackState: () => {
@@ -44,6 +45,10 @@ const spotifyInvalidMock = {getMyCurrentPlaybackState: () => {
 /* MAIN APP TESTS */
 describe('App component tests', () => {
 
+beforeEach(() => {
+    jest.resetModules();
+});
+
 it('App component renders correctly', () => {
     const container = render(<App/>);
     expect(container.firstChild).toMatchSnapshot();
@@ -51,25 +56,32 @@ it('App component renders correctly', () => {
 
 it('Test valid spotify api', () => {
     const validSpotifyApp = mount(<App/>);
-    global.spotifyWebApi = spotifyMock;
     validSpotifyApp.instance().spotifyWebApi = spotifyMock;
     expect(validSpotifyApp.instance().state.nowPlaying.name).toEqual('Not Checked');
-    validSpotifyApp.instance().getNowPlaying();
-    //expect(validSpotifyApp.instance().state.nowPlaying.name).toBe('Pusha T');    
+    validSpotifyApp.instance().getNowPlaying();  
 });
 
-it('Successfully gets reccomendations', () => {
-    const appRec = mount(<App/>);
-    appRec.instance().spotifyWebApi = spotifyMock;
-    appRec.instance().getRecommendations();
+it('assert no errors in methods', () => {
+    const validSpotifyApp = mount(<App/>);
+    validSpotifyApp.instance().refresh();
+    validSpotifyApp.instance().defaultTheme();
+    validSpotifyApp.instance().setColor();
+    validSpotifyApp.instance().updateTheme();
+    validSpotifyApp.instance().themeModeToggle();
+    validSpotifyApp.instance().getNextSong();
+    validSpotifyApp.instance().getLastSong();
+    validSpotifyApp.instance().getPause();
+    validSpotifyApp.instance().getShuffle();
 });
 
-// it('Test valid spotify api with no artist profile', () => {
-//     const validSpotifyApp = mount(<App/>);
-//     validSpotifyApp.instance().spotifyWebApi = spotifyMock;
-//     validSpotifyApp.instance().ArtistProfile = undefined;
-//     validSpotifyApp.instance().getNowPlaying();
-// });
+it('test hotkeys', () => {
+    const validSpotifyApp = mount(<App/>);
+    validSpotifyApp.instance().onKeyDown('left', undefined, undefined);
+    validSpotifyApp.instance().onKeyDown('right', undefined, undefined);
+    validSpotifyApp.instance().onKeyDown('space', undefined, undefined);
+    validSpotifyApp.instance().onKeyDown('s', undefined, undefined);
+    validSpotifyApp.instance().onKeyDown('r', undefined, undefined);
+});
 
 it('Test with spotify api with invalid song objects', () => {
     const validSpotifyApp = mount(<App/>);
@@ -149,4 +161,19 @@ it('Test invalid song', () => {
     expect(wrapperInvalidSong.instance().getArtist()).rejects.toEqual('Error: Invalid Song Object');
 });
 
+});
+
+describe('Lyrics component tests', () => {
+
+const lyricsWrapper = shallow(<Lyrics spotifyApi={spotifyMock} onRef={ref => (undefined)}/>);
+lyricsWrapper.instance().refreshArtist();
+
+it('Lyrics component renders correctly', () => {
+    const container = render(<Lyrics spotifyApi={undefined} onRef={ref => (undefined)}/>);
+    expect(container.firstChild).toMatchSnapshot();
+});
+
+it('Spotify API is being properly read', () => {
+    expect(lyricsWrapper.instance().props.spotifyApi).not.toBe(undefined);
+});
 });
