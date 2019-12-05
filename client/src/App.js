202f7480203a4ce74c7180cbd8cf36711f8baada
camfarms <ccf35@case.edu>
2019-12-04 21:18:26 -0500

    Changed homepage value to Tortoise, and removed /login from login button's redirect

--- client/src/App.js
+++ client/src/App.js
@@ -313,7 +313,7 @@ class App extends Component {
         <div>
           <NavBar />
         </div>
-        <a href='http://localhost:4002/login'> 
+        <a href='http://localhost:4002'> 
         <Button variant="contained" color="primary">Login with Spotify</Button> 
         </a>
         <div>
