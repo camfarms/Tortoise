d9550b0a27c6649db533433983948391636e9613
camfarms <ccf35@case.edu>
2019-12-06 15:49:51 -0500

    Fixed login button label

---
+++ client/build/precache-manifest.ca8d5812a57e708706a47a23adb4283c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e028dc6ea09f3436f9af5cd9dcd94ff0",
+    "url": "/Tortoise/index.html"
+  },
+  {
+    "revision": "d7ca00d7f778a9ad29bc",
+    "url": "/Tortoise/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e48fb391d1430c2ddd9e",
+    "url": "/Tortoise/static/css/main.aed36a2a.chunk.css"
+  },
+  {
+    "revision": "d7ca00d7f778a9ad29bc",
+    "url": "/Tortoise/static/js/2.324cefee.chunk.js"
+  },
+  {
+    "revision": "e48fb391d1430c2ddd9e",
+    "url": "/Tortoise/static/js/main.d317ea6e.chunk.js"
+  },
+  {
+    "revision": "758659570612934c42eb",
+    "url": "/Tortoise/static/js/runtime-main.8e768d33.js"
+  },
+  {
+    "revision": "b97c5779ae783ea517d93dd4d2a95a0c",
+    "url": "/Tortoise/static/media/tortoise_white.b97c5779.png"
+  }
+]);
